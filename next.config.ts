87fb6014c5f2bb8300d4ed687cda5fb8f9d978cc
rkhansa053 <160234986+rkhansa053@users.noreply.github.com>
2025-01-04 18:57:33 +0500

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,16 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images:{
+    domains:['']
+  }
+};
+
+export default nextConfig;
+
+
+module.exports = {
+  images: {
+      domains: ['example.com'], // Replace with your image host
+  },
+};
